84db9fca758d215dcf558352536985d0b1dff31d
pieash9 <pieash9@gmail.com>
2025-03-24 22:21:42 +0600

    creating worker

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
 //  dependencies
-const http = require("http");
-const { handleReqRes } = require("./helpers/handleReqRes");
-const environment = require("./helpers/environments");
-const lib = require("./lib/data");
-const { sendTwilioSms } = require("./helpers/notifications");
+const server = require("./lib/server");
+const workers = require("./lib/worker");
 
 // app object - module scafolding
 const app = {};
 
-// TODO Remove  later
-// sendTwilioSms("01627001665", "Hello world", (err) => {
-//   console.log(err);
-// });
-
-// configuration
-app.config = {
-  port: 3000,
+app.init = () => {
+  // start the server
+  server.init();
+  // start the workers
+  workers.init();
 };
 
-// create server
-app.createServer = () => {
-  const server = http.createServer(app.handleReqRes);
+app.init();
 
-  server.listen(environment.port, () => {
-    console.log("listening to port " + environment.port);
-  });
-};
-
-// handle request Response
-app.handleReqRes = handleReqRes;
-// start the server
-app.createServer();
+module.exports = app;
