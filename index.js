1585511e69fb5842db329c0298d3aa6818c22b5b
Md. Pieash Ahmed <pieash9@gmail.com>
2025-03-25 15:01:16 +0600

    added req body

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.raw());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.post("/", (req, res) => {
+  console.log(req.body.name);
   res.send("Hello World from post request!");
 });
 
