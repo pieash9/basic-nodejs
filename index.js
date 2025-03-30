e1cf70ff305dd19c75c2188e2b0d33c24a1c38a2
Md. Pieash Ahmed <pieash9@gmail.com>
2025-03-30 17:43:53 +0600

    added routing basic

--- index.js
+++ index.js
@@ -1,45 +1,11 @@
 const express = require("express");
-const cookieParser = require("cookie-parser");
+const adminRouter = require("./adminRouter");
+const publicRouter = require("./publicRouter");
 
 const app = express();
-app.use(express.json());
-const adminRouter = express.Router();
 
-app.use(cookieParser());
-
-const loggerWrapper = (options) => {
-  return function (req, res, next) {
-    if (options.log) {
-      console.log(
-        `${new Date(Date.now()).toLocaleString()} - ${req.method} - ${
-          req.originalUrl
-        } - ${req.protocol} - ${req.ip}`
-      );
-      next();
-    } else {
-      throw new Error("Server side error!");
-    }
-  };
-};
-
-adminRouter.use(loggerWrapper({ log: false }));
-
-adminRouter.get("/dashboard", (req, res) => {
-  res.send("Admin Dashboard");
-});
-
+app.use("/", publicRouter);
 app.use("/admin", adminRouter);
-
-app.get("/about", (req, res) => {
-  res.send("About");
-});
-
-const errorMiddleware = (err, req, res, next) => {
-  console.log(err.message);
-  res.status(500).send("Server side error!");
-};
-
-adminRouter.use(errorMiddleware);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
